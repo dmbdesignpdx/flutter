e59f826447964ea86cce9695c47550b6350c3642
Daniel Blake <daniel@danielblake.design>
2020-01-21 15:56:46 -0800

    improves animation

--- lib/ui/card.dart
+++ lib/ui/card.dart
@@ -3,55 +3,78 @@ import 'package:flutter/material.dart';
 import '../utils/animation.dart';
 
 
-// Card
+// AppCard
 class AppCard extends StatefulWidget {
+  final String title;
+  final String terrain;
+  final String climate;
+  final String population;
+  
   const AppCard({
     @required Key key,
     @required this.title,
-    @required this.copy,
+    @required this.terrain,
+    @required this.climate,
+    @required this.population,
   }) : super(key: key);
-
-  final String title;
-  final String copy;
 
   @override
   State createState() => _AppCardState();
 }
 
-// Card State
-class _AppCardState extends State<AppCard> {
-  // State
+// AppCard State
+class _AppCardState extends State<AppCard> with SingleTickerProviderStateMixin {
+  // States
   bool _initial = true;
+  bool _showText = false;
 
+  // State Methods
+  void _cardExpand() {
+    if (_initial) {
+      Future.delayed(Duration(milliseconds: 300), () {
+        setState(() {
+          _showText = true;
+        });
+      });
+    } else {
+      setState(() {
+        _showText = false;
+      });
+    }
+    
+    setState(() {
+      _initial = !_initial;
+    });
+  }
+
+  // Widget
   @override
   Widget build(BuildContext context) => GestureDetector(
     // Actions
     onTap: () {
-      setState(() {
-        _initial = !_initial;
-      });
+      _cardExpand();
     },
 
     // Style
     child: AnimatedContainer(
       duration: AppAnimate.duration,
       curve: AppAnimate.curve,
-      height: _initial ? 100 : 200,
+      height: _initial ? 90 : 260,
       margin: const EdgeInsets.fromLTRB(18, 18, 18, 0),
       padding: const EdgeInsets.all(30),
       decoration: BoxDecoration(
-        color: _initial ? Colors.grey[50] : Colors.white,
-        borderRadius: BorderRadius.circular(10),
+        color: _initial ? Colors.grey[100] : Colors.grey[50],
+        borderRadius: BorderRadius.circular(6),
         boxShadow: <BoxShadow>[
           BoxShadow(
-            color: const Color(0x22000033),
-            offset: _initial ? const Offset(0, 2) : const Offset(0, 6),
-            blurRadius: _initial ? 3 : 9,
+            color: const Color(0x22000044),
+            offset: _initial ? const Offset(0, 0) : const Offset(0, 6),
+            blurRadius: _initial ? 0 : 12,
           ),
         ],
       ),
 
-      // Child
+      // Content
       child: Column(
         children: <Widget>[
 
@@ -68,19 +91,47 @@ class _AppCardState extends State<AppCard> {
             ),
           ),
 
-          // Terrain
+          // Inner Text
           AnimatedOpacity(
-            opacity: _initial ? 0 : 1,
             duration: AppAnimate.duration,
             curve: AppAnimate.curve,
-            child: AnimatedContainer(
-              duration: AppAnimate.duration,
-              curve: AppAnimate.curve,
-              height: _initial ? 0 : 60,
-              margin: EdgeInsets.only(top: 10),
-              child: Text(
-                'Terrain: ${widget.copy}',
-                style: Theme.of(context).textTheme.body1,
+            opacity: !_showText ? 0 : 1,
+            child: Container(
+              height: !_showText ? 0 : 160,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+
+                  // Terrain
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Terrain: ${widget.terrain}',
+                      style: Theme.of(context).textTheme.body1,
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+
+                  // Climate
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Climate: ${widget.climate}',
+                      style: Theme.of(context).textTheme.body1,
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  // Climate
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Population: ${widget.population}',
+                      style: Theme.of(context).textTheme.body1,
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                
+                ],
               ),
             ),
           ),
